4cba1e5873daba440113ca0462b6e51d74b407f2
TelesReis <r0drig0t3l3s@hotmail.com>
2022-02-04 10:36:26 -0300

    Camada de serviço, registro de componentes

---
+++ src/main/java/com/telesreis/projet/services/UserService.java
@@ -0,0 +1,26 @@
+package com.telesreis.projet.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.telesreis.projet.entities.User;
+import com.telesreis.projet.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findByID(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
