4e82f561cbc2d7c09d916b48c175ec286655f6ec
TelesReis <r0drig0t3l3s@hotmail.com>
2022-02-07 18:34:06 -0300

    Pagamento, associação individual

--- src/main/java/com/telesreis/projet/config/TestConfig.java
+++ src/main/java/com/telesreis/projet/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.telesreis.projet.entities.Category;
 import com.telesreis.projet.entities.Order;
 import com.telesreis.projet.entities.OrderItem;
+import com.telesreis.projet.entities.Payment;
 import com.telesreis.projet.entities.Product;
 import com.telesreis.projet.entities.User;
 import com.telesreis.projet.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
