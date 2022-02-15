89e63668ddc105abe7b8ad752f60d4e2fe5df8a4
TelesReis <r0drig0t3l3s@hotmail.com>
2022-02-15 08:38:54 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/telesreis/projet/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.telesreis.projet.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
